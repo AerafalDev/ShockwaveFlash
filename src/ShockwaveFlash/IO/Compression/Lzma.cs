e6a6a56a9be3f64745db84ce458695e8e7995e0d
Aerafal <jimmy.mll@hotmail.com>
2026-02-25 19:13:13 +0100

    feat: add LZMA and ZLib decompression utilities

---
+++ src/ShockwaveFlash/IO/Compression/Lzma.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Aerafal 2026.
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace ShockwaveFlash.IO.Compression;
+
+public static class Lzma
+{
+    public static ReadOnlySpan<byte> Decompress(ref readonly ReadOnlySpan<byte> compressed, int uncompressedLength)
+    {
+        throw new NotSupportedException(
+            "ZWS (LZMA-compressed) SWF files require an LZMA decoder. " +
+            "SPEC GAP: .NET 10 BCL does not include a built-in LZMA implementation. " +
+            "Add a NuGet reference to a library providing LZMA support (e.g. SharpCompress) " +
+            "and replace this method body with the appropriate decompression call.");
+    }
+}
---
+++ src/ShockwaveFlash/IO/Compression/ZLib.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Aerafal 2026.
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.IO.Compression;
+
+namespace ShockwaveFlash.IO.Compression;
+
+public static class ZLib
+{
+    private const int MaxDecompressedSize = 512 * 1024 * 1024;
+
+    public static unsafe ReadOnlySpan<byte> Decompress(ref readonly ReadOnlySpan<byte> compressed, int uncompressedLength)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(uncompressedLength);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(uncompressedLength, MaxDecompressedSize);
+
+        var decompressed = new byte[uncompressedLength];
+        var decompressedSpan = decompressed.AsSpan();
+
+        fixed (byte* ptr = &compressed.GetPinnableReference())
+        {
+            using var ms = new UnmanagedMemoryStream(ptr, compressed.Length);
+            using var zs = new ZLibStream(ms, CompressionMode.Decompress, false);
+
+            var reads = 0;
+
+            while (reads < uncompressedLength)
+            {
+                var bytesRead = zs.Read(decompressedSpan[reads..]);
+
+                if (bytesRead is 0)
+                    break;
+
+                reads += bytesRead;
+            }
+
+            if (reads != uncompressedLength)
+                throw new InvalidOperationException($"Expected {uncompressedLength} bytes but decompressed {reads} bytes.");
+
+            return decompressedSpan[..reads];
+        }
+    }
+}
