b6ccae9a5bf95342b32956595149298eb34e70c5
Aerafal <jimmy.mll@hotmail.com>
2026-02-25 18:38:32 +0100

    feat: add BinaryPrimitivesExtensions for handling 24-bit integer reads

---
+++ src/ShockwaveFlash/IO/Extensions/BinaryPrimitivesExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Aerafal 2026.
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Buffers.Binary;
+using System.Runtime.CompilerServices;
+
+namespace ShockwaveFlash.IO.Extensions;
+
+internal static class BinaryPrimitivesExtensions
+{
+    extension(BinaryPrimitives)
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint ReadUInt24LittleEndian(in ReadOnlySpan<byte> source)
+        {
+            return source[0] | ((uint)source[1] << 8) | ((uint)source[2] << 16);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int ReadInt24LittleEndian(in ReadOnlySpan<byte> source)
+        {
+            return ((source[0] | (source[1] << 8) | (source[2] << 16)) << 8) >> 8;
+        }
+    }
+}
