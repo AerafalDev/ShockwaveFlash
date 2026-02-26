f79b6387227e3038e9471371091627536d55517c
Aerafal <jimmy.mll@hotmail.com>
2026-02-27 00:44:02 +0100

    feat: add foundational AVM1 classes and exceptions for SWF action handling

---
+++ src/ShockwaveFlash/Actions/Avm1NativeFunction.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Aerafal 2026.
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace ShockwaveFlash.Actions;
+
+public class Avm1NativeFunction
+{
+    
+}
