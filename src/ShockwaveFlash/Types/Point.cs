a25d3a047440b92e1fde02f3a126fa1221ad84e3
Aerafal <jimmy.mll@hotmail.com>
2026-02-26 08:55:10 +0100

    feat: add Point and Rectangle structs to represent geometric types

---
+++ src/ShockwaveFlash/Types/Point.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Aerafal 2026.
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace ShockwaveFlash.Types;
+
+[DebuggerDisplay("{ToString(),nq}")]
+[StructLayout(LayoutKind.Sequential)]
+public struct Point :
+    IFormattable,
+    IEquatable<Point>,
+    IEqualityOperators<Point, Point, bool>
+{
+    public int X;
+
+    public int Y;
+
+    public Point(int value) : this(value, value)
+    {
+    }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public bool Equals(Point other)
+    {
+        return X == other.X && Y == other.Y;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Point other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
+
+    public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format, IFormatProvider? formatProvider)
+    {
+        return $"{X.ToString(format, formatProvider)}, {Y.ToString(format, formatProvider)}";
+    }
+
+    public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format)
+    {
+        return ToString(format, CultureInfo.CurrentCulture);
+    }
+
+    public override string ToString()
+    {
+        return ToString("G", CultureInfo.CurrentCulture);
+    }
+
+    public static bool operator ==(Point left, Point right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Point left, Point right)
+    {
+        return !left.Equals(right);
+    }
+}
---
+++ src/ShockwaveFlash/Types/Rectangle.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) Aerafal 2026.
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using ShockwaveFlash.IO.Binary;
+using ShockwaveFlash.IO.Bits;
+using ShockwaveFlash.Types.Abstractions;
+
+namespace ShockwaveFlash.Types;
+
+[DebuggerDisplay("{ToString(),nq}")]
+[StructLayout(LayoutKind.Sequential)]
+public struct Rectangle :
+    IFormattable,
+    IEquatable<Rectangle>,
+    IEqualityOperators<Rectangle, Rectangle, bool>,
+    IReadable<Rectangle>
+{
+    public int XMin;
+
+    public int XMax;
+
+    public int YMin;
+
+    public int YMax;
+
+    public int Width =>
+        Math.Max(XMax - XMin, 0);
+
+    public int Height =>
+        Math.Max(YMax - YMin, 0);
+
+    public Point TopLeft =>
+        new(XMin, YMin);
+
+    public Point BottomRight =>
+        new(XMax, YMax);
+
+    public Point TopRight =>
+        new(XMax, YMin);
+
+    public Point BottomLeft =>
+        new(XMin, YMax);
+
+    public Point Center =>
+        new(XMin + Width / 2, YMin + Height / 2);
+
+    public Rectangle(int xMin, int xMax, int yMin, int yMax)
+    {
+        XMin = xMin;
+        XMax = xMax;
+        YMin = yMin;
+        YMax = yMax;
+    }
+
+    public bool Equals(Rectangle other)
+    {
+        return XMin == other.XMin && XMax == other.XMax && YMin == other.YMin && YMax == other.YMax;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Rectangle other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(XMin, XMax, YMin, YMax);
+    }
+
+    public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format, IFormatProvider? formatProvider)
+    {
+        return $"{XMin.ToString(format, formatProvider)}..{XMax.ToString(format, formatProvider)}, {YMin.ToString(format, formatProvider)}..{YMax.ToString(format, formatProvider)}";
+    }
+
+    public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format)
+    {
+        return ToString(format, CultureInfo.CurrentCulture);
+    }
+
+    public override string ToString()
+    {
+        return ToString("G", CultureInfo.CurrentCulture);
+    }
+
+    public static bool operator ==(Rectangle left, Rectangle right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Rectangle left, Rectangle right)
+    {
+        return !left.Equals(right);
+    }
+
+    public static Rectangle Read(ref SpanReader reader)
+    {
+        var bits = new BitReader();
+
+        var nBits = bits.ReadIBits(ref reader, 5);
+
+        var xMin = bits.ReadSBits(ref reader, nBits);
+        var xMax = bits.ReadSBits(ref reader, nBits);
+        var yMin = bits.ReadSBits(ref reader, nBits);
+        var yMax = bits.ReadSBits(ref reader, nBits);
+
+        return new Rectangle(xMin, xMax, yMin, yMax);
+    }
+}
