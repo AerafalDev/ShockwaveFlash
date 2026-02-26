3f2a911995c57d748ee220e89068e7d9943052d4
Aerafal <jimmy.mll@hotmail.com>
2026-02-26 09:45:27 +0100

    feat: add global usings

---
+++ src/ShockwaveFlash/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Aerafal 2026.
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+global using SpanReader = ShockwaveFlash.IO.Binary.SpanReader;
+global using BitReader = ShockwaveFlash.IO.Bits.BitReader;
