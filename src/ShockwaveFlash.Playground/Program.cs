91ed67ce65a6c3f89726ed696a1875c9a9eac853
Aerafal <jimmy.mll@hotmail.com>
2026-02-28 11:38:14 +0100

    feat: add SWF disassembly and AVM1 action decoding to `ShockwaveFlash.Playground`

--- src/ShockwaveFlash.Playground/Program.cs
+++ src/ShockwaveFlash.Playground/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ShockwaveFlash;
+using ShockwaveFlash.IO.Binary;
+using ShockwaveFlash.Tags.Action;
 
-Console.WriteLine("Hello, World!");
+var buffer = File.ReadAllBytes(args[0]);
+
+var reader = new SpanReader(buffer);
+
+var swf = ShockwaveFlashFile.Disassemble(ref reader);
+
+foreach (var doActionTag in swf.Tags.OfType<DoActionTag>())
+{
+    var actions = doActionTag.DecodeActions(ref reader, swf.Header.Version);
+
+    foreach (var action in actions)
+        Console.WriteLine(action);
+}
