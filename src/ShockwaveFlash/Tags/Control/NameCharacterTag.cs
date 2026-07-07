58545926b7b3a3257e805331ed24fec5eca382ee
Aerafal <jimmy.mll@hotmail.com>
2026-07-07 23:24:08 +0200

    feat: add `Type` property to `NameCharacterTag` for extended type support during decoding and encoding

--- src/ShockwaveFlash/Tags/Control/NameCharacterTag.cs
+++ src/ShockwaveFlash/Tags/Control/NameCharacterTag.cs
@@ -6,23 +6,33 @@ public sealed class NameCharacterTag : Tag
 
     public string Name { get; set; }
 
-    public NameCharacterTag(TagMetadata metadata, ushort id, string name) : base(metadata)
+    public ushort? Type { get; set; }
+
+    public NameCharacterTag(TagMetadata metadata, ushort id, string name, ushort? type) : base(metadata)
     {
         Id = id;
         Name = name;
+        Type = type;
     }
 
     public static NameCharacterTag Decode(MemoryReader reader, TagMetadata metadata)
     {
         var id = reader.ReadUInt16();
         var name = reader.ReadNullTerminatedString();
+        ushort? type = null;
 
-        return new NameCharacterTag(metadata, id, name);
+        if (reader.Remaining > 0)
+            type = reader.ReadUInt16();
+
+        return new NameCharacterTag(metadata, id, name, type);
     }
 
     public override void Encode(MemoryWriter writer, byte swfVersion)
     {
         writer.WriteUInt16(Id);
         writer.WriteNullTerminatedString(Name);
+
+        if (Type.HasValue)
+            writer.WriteUInt16(Type.Value);
     }
 }
